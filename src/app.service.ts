0eba2280d9065cf2136f93319e95755e768a3e23
zy <varzy@live.com>
2022-10-12 16:17:30 +0800

    微调基础接口

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import AppConfig from './config/app.config';
 
 @Injectable()
 export class AppService {
+  private appConfig: AppConfig;
+
+  constructor(protected readonly configService: ConfigService) {
+    this.appConfig = configService.get<AppConfig>('app');
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return `hello, world | ${process.env.APP_ENV}`;
   }
 }
